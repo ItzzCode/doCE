8cf7ef4018cea2c5dd3907a20fbced222f36e078
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-11-24 19:05:00 -0500

    funky reverts

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <tice.h>
+#include <stdio.h>
+
+int main(void){
+	os_ClrHome();
+	
+	os_SetCursorPos(0,6);
+	os_PutStrFull("Welcome to doCE!");
+	if (boot_IsAfterNoon()){
+		os_SetCursorPos(1,6);
+		os_PutStrFull("Good afternoon!");
+	} else {
+		os_SetCursorPos(1,7);
+		os_PutStrFull("Good morning!");
+	}
+	sleep(5);
+}
