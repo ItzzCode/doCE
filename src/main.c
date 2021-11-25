834154da7f675d9172e3586242fa40b26fe07d39
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-11-25 00:24:14 -0500

    This is trash
    i give up

--- src/main.c
+++ src/main.c
@@ -1,11 +1,45 @@
 #include <tice.h>
+#include <graphx.h>
 #include <stdio.h>
+#include <stdarg.h>
+
+void pause(void){
+    while(!os_GetCSC());
+}
+
+//painnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+void menu(char title[26], char opt1[26], char opt2[26], char opt3[26], char opt4[26], char opt5[26]){
+	gfx_Begin();
+	gfx_SetTextScale(2,2);
+	os_ClrLCDFull();
+	bool undecided = true;
+	int selection = 0;
+	while(undecided){
+		gfx_PrintStringXY(title,0,0);
+		gfx_PrintStringXY(opt1,1,0);
+		undecided=false;
+		pause();
+		gfx_End();
+    }
+    /*
+    while `undecided`
+        var int selection
+        output title
+        output options with selection
+        if arrow pressed
+            change selection
+        if enter pressed
+            go to function determined by args
+    */
+}
 
 int main(void){
 	os_ClrHome();
 	
+	/*greet user*/
 	os_SetCursorPos(0,6);
 	os_PutStrFull("Welcome to doCE!");
+	/*by time*/
 	if (boot_IsAfterNoon()){
 		os_SetCursorPos(1,6);
 		os_PutStrFull("Good afternoon!");
@@ -13,5 +47,6 @@ int main(void){
 		os_SetCursorPos(1,7);
 		os_PutStrFull("Good morning!");
 	}
-	while(!os_GetCSC());
+	pause();
+	menu("title","1","","","","");
 }
